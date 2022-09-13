38b61c37b20159b27426d4551fbd478fe6a2c1d7
nickherrig <neherrig@gmail.com>
2022-09-13 18:20:12 -0500

    Refactor class Square component to function component

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,25 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null,
-    }
-  }
 
-  render() {
-    return (
-      <button 
-      className="square" 
-      onClick={() => {this.setState({value: 'X'})}}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  const  [value, setValue] = useState();
+  return (
+    <button 
+    className="square" 
+    onClick={ () => setValue('X') }>
+      {value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
